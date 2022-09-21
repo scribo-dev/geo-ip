0d8081da51d7ba0de4f179c574ca35db5fec7bfa
Renan Lopes Ferreira <relferreira@gmail.com>
2022-09-21 18:01:06 -0300

    Closest City API

--- pages/api/location.ts
+++ pages/api/location.ts
@@ -1,9 +1,33 @@
 import { NextRequest } from "next/server";
+import { distance } from "../../lib/distance";
 
 export const config = {
   runtime: "experimental-edge",
 };
 
 export default async function handler(req: NextRequest) {
-  return new Response(JSON.stringify(req.geo));
+  switch (req.method) {
+    case "POST":
+      const { locations } = await req.json();
+      const geo = req.geo;
+
+      let closestCity, closestDistance;
+      for (let location of locations) {
+        let closest = distance(
+          geo.latitude,
+          geo.longitude,
+          location.latitude,
+          location.longitude,
+          "K"
+        );
+
+        if (!closest || closest < closestDistance) {
+          closestDistance = closest;
+          closestCity = location;
+        }
+      }
+      return new Response(JSON.stringify({ closestCity }));
+    default:
+      return new Response(JSON.stringify(req.geo));
+  }
 }
